51fcec85c1cfb4bf71e85aa245b0f252c35a97c2
SamSvetlik <sam.svetlik@gmail.com>
2022-10-29 21:57:40 -0500

    trying and failing to add like button

--- src/App.js
+++ src/App.js
@@ -27,9 +27,19 @@ class App extends Component {
       }
 
       handleClick = (index) => {
+        // My first idea was to give each beer an "isLiked" property on import
+        // I was able to do this, but then was unable to change it.
+        // this.setState({arrayOfBeers[index].isLiked: true}) <-- Doesn't work. Syntax error?
+
+        // My next idea was to grab the beer object, make a copy, and append the beer object to the end of the array.
         console.log(this.state.arrayOfBeers[index])
+        // Successfully grabs the beer object
         let copy = [...this.state.likedBeers, this.state.arrayOfBeers[index]]
-        this.setState({likedBeers: copy})
+        // The copy returns as expected, an array with the beer object appended.
+        console.log(copy)
+        // But when I use setState, likedBeers does not become the copy.
+        this.setState({ likedBeers: copy })
+        console.log(this.state.likedBeers)
       }
 
         render() {
